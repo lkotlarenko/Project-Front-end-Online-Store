40614bad9271ba68104daa6d655e13c87457bc16
Hylea <matheusalmeidaf96@gmail.com>
2022-01-20 17:09:09 -0300

    Completa o requisito 04.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,46 @@
 import React, { Component } from 'react';
 import CartBtn from '../components/CartBtn';
+import * as api from '../services/api';
 
 export class Home extends Component {
+  constructor() {
+    super();
+    this.state = {
+      categories: [],
+    };
+  }
+
+  componentDidMount() {
+    this.setCategories();
+  }
+
+  setCategories = async () => {
+    const data = await api.getCategories();
+    this.setState({
+      categories: data,
+    });
+  }
+
   render() {
+    const { categories } = this.state;
     return (
       <main>
         <span data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </span>
         <CartBtn />
+        <p>
+          {categories.map((item) => (
+            <label key={ item.id } htmlFor={ item.id } data-testid="category">
+              <input
+                type="radio"
+                name="category"
+                id={ item.id }
+              />
+              { item.name }
+            </label>
+          ))}
+        </p>
       </main>
     );
   }
